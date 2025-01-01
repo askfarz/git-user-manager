5cc32a626250d4369ade801aa1a7f1e4cf2ee0dd
Farkhad <askarovfarkhad.96@gmail.com>
2025-01-01 22:07:30 +0300

    edited plugin settings

--- src/main/kotlin/askfar/com/gitusermanager/action/GitUserManagerAction.kt
+++ src/main/kotlin/askfar/com/gitusermanager/action/GitUserManagerAction.kt
@@ -3,15 +3,18 @@ package askfar.com.gitusermanager.action
 import askfar.com.gitusermanager.ui.GitUserManagerDialog
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.application.ApplicationManager
 import mu.KotlinLogging
 
 class GitUserManagerAction : AnAction() {
 
     private val logger = KotlinLogging.logger {}
-    private val dialog = GitUserManagerDialog()
 
     override fun actionPerformed(e: AnActionEvent) {
         logger.trace { "Handling the GitUserManagerAction" }
-        dialog.show()
+        ApplicationManager.getApplication().invokeLater {
+            val dialog = GitUserManagerDialog()
+            dialog.show()
+        }
     }
 }
